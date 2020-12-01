bc9ec0ca03a2bdd521b599a13d28c00a57bfbbd0
Olovyannikov <excaliburgyro@gmail.com>
2020-12-01 09:50:32 +0300

    Начал делать drilldown меню

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -2,11 +2,21 @@ export default () => {
     let header = document.querySelector(`.js-header`);
     let menuToggler = document.querySelector(`.js-menu-toggler`);
     let menuLinks = document.querySelectorAll(`.js-menu-link`);
+    let menuList = document.querySelector(".navigation__list");
     let menuItems = document.querySelectorAll(".navigation__item");
+    let backBtn = document.querySelector(".navigation__back");
 
     for (let i = 0; i < menuItems.length; i++) {
         menuItems[i].onclick = function () {
-            menuItems[i].querySelector(".navigation__list").classList.add('navigation__submenu--active');
+            if (menuItems[i].classList.contains("navigation__item--back")) {
+                console.log("123");
+            } else {
+                if (menuItems[i].querySelector(".navigation__list")) {
+                    menuItems[i]
+                        .querySelector(".navigation__list")
+                        .classList.add("navigation__submenu--active");
+                }
+            }
         };
     }
 
@@ -15,9 +25,9 @@ export default () => {
             if (header.classList.contains(`header--menu-opened`)) {
                 header.classList.remove(`header--menu-opened`);
                 document.body.classList.remove(`menu-opened`);
-                menuItems.forEach(item => {
-                    item.querySelector('.navigation__list').classList.remove('navigation__submenu--active')
-                })
+                menuItems.forEach((item) => {
+                    if(menu)
+                });
             } else {
                 header.classList.add(`header--menu-opened`);
                 document.body.classList.add(`menu-opened`);
@@ -32,5 +42,25 @@ export default () => {
                 document.body.classList.remove(`menu-opened`);
             }
         });
+    }
+
+    const find = (node, parent) => {
+        while (node) {
+            if (node.classList.contains(parent)) {
+                return node;
+            } else {
+                node = node.parentElement;
+            }
+        }
+
+        return null;
+    };
+
+    for (let j = 0; j < backBtn.length; j++) {
+        backBtn.onclick = () => {
+            find(".navigation__back", ".navigation__submenu").classList.remove(
+                "navigation__submenu--active"
+            );
+        };
     }
 };
