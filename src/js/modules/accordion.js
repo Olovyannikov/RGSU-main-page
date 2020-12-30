178ad662cbd22245a888aa05281ad2c75746bfc1
Оловянников Илья Владимирович <OloviannikovIV@rgsu.net>
2020-12-30 10:12:01 +0300

    Поправил баги в соответствии с issue в trello от 29.12.2020

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -6,11 +6,16 @@ export default () => {
 
         for (let i = 0; i < items.length; i++) {
             items[i].setAttribute('aria-expanded', 'false');
+            items[i].onclick = () => {
+                items[i].classList.toggle('accordion-button--active')
+            }
         }
 
         if (itemToggle === 'false') {
             this.setAttribute('aria-expanded', 'true');
         }
+
+
     }
 
     items.forEach(item => item.addEventListener('click', toggleAccordion));
