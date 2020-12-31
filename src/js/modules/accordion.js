f97699e2ce4d87fd964fc943cdda7504c5cc427c
Ilya Olovyannikov <olovyannikov_barista@icloud.com>
2020-12-31 16:06:43 +0300

    поправил аккордеон, пофиксил меню по правкам от 30.12.20

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,14 +1,18 @@
 export default () => {
     const items = document.querySelectorAll(".accordion button");
 
+    for (let i = 0; i < items.length; i++) {
+        items[i].addEventListener('click', () => {
+            items[i].classList.toggle('accordion-button--active')
+        });
+    }
+
     function toggleAccordion() {
         const itemToggle = this.getAttribute('aria-expanded');
 
         for (let i = 0; i < items.length; i++) {
             items[i].setAttribute('aria-expanded', 'false');
-            items[i].onclick = () => {
-                items[i].classList.toggle('accordion-button--active')
-            }
+
         }
 
         if (itemToggle === 'false') {
